32d9ec562b741f9d6498401c7c5ce75eee19d25f
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-03 21:10:13 +0000

    Home Page: Creating the Testimonial Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,10 +33,10 @@
         <div class="my-24 mx-auto max-w-screen-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             <div
                 class="m-2 group px-10 py-5 bg-white/10 rounded-lg flex flex-col items-center justify-center gap-2 relative after:absolute after:h-full after:bg-secondary z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0 [&amp;_p]:delay-200 [&amp;_p]:transition-all">
-                <img src={{asset('assets/images/quarter_pounder.png')}} alt="quarter_pounder">
+                <img src={{ asset('assets/images/quarter_pounder.png') }} alt="quarter_pounder">
 
                 <p class="cardtxt font-semibold text-gray-200 tracking-wider group-hover:text-gray-700 text-xl">
-                    Quarter Pounder®* with Cheese 
+                    Quarter Pounder®* with Cheese
                 </p>
                 <p class="blueberry font-semibold text-gray-400 group-hover:text-gray-600 text-xs">
                     One of Kind &amp; Unique Plants Collection Here at SMKY.
@@ -147,46 +147,109 @@
         <div class="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16">
             <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
                 <div class="w-full h-64 rounded-lg shadow-md">
-                    <img src={{asset('assets/images/hamburger.jpg')}} class="rounded-lg w-full h-full" alt="hamburger">
+                    <img src={{ asset('assets/images/hamburger.jpg') }} class="rounded-lg w-full h-full" alt="hamburger">
                 </div>
-            
+
                 <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
-                    <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">Hamburger</h3>
-            
+                    <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">Hamburger
+                    </h3>
+
                     <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
                         <span class="font-bold text-gray-800 dark:text-gray-200">20 Items</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button
+                            class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add
+                            to cart</button>
                     </div>
                 </div>
             </div>
             <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
                 <div class="w-full h-64 rounded-lg shadow-md">
-                    <img src={{asset('assets/images/Pizza.jpg')}} class="rounded-lg w-full h-full" alt="Pizza">
+                    <img src={{ asset('assets/images/Pizza.jpg') }} class="rounded-lg w-full h-full" alt="Pizza">
                 </div>
-            
+
                 <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
                     <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">Pizza</h3>
-            
+
                     <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
                         <span class="font-bold text-gray-800 dark:text-gray-200">123 Items</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button
+                            class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add
+                            to cart</button>
                     </div>
                 </div>
             </div>
             <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
                 <div class="w-full h-64 rounded-lg shadow-md">
-                    <img src={{asset('assets/images/BBQ.jpg')}} class="rounded-lg w-full h-full" alt="Pizza">
+                    <img src={{ asset('assets/images/BBQ.jpg') }} class="rounded-lg w-full h-full" alt="Pizza">
                 </div>
-            
+
                 <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
                     <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">BBQ</h3>
-            
+
                     <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
                         <span class="font-bold text-gray-800 dark:text-gray-200">10 Items</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button
+                            class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add
+                            to cart</button>
                     </div>
                 </div>
             </div>
         </div>
+    </section>
+    <section class="w-screen h-screen top-1">
+        <div class="w-full h-full flex flex-col items-center justify-center">
+            <img src="{{ asset('assets/images/Testimonials.png') }}" alt="hero" class="w-full h-full object-cover" />
+            <div class="absolute w-full h-full flex flex-col items-center justify-around">
+                <div class="flex flex-col justify-center">
+                    <h1 class="text-5xl font-bold font-passero text-center mb-4 text-white "> Our Valuable Clients</h1>
+                    <p class="text-xl max-w-4xl font-passero text-center text-neutral-700 dark:text-neutral-300">What
+                        people are saying about us ?</p>
+                </div>
+
+                <figure class="max-w-screen-lg self-center mx-auto px-8 lg:px-0 text-center">
+                    <figcaption
+                        class="flex flex-col gap-12 items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+                        <div class="flex w-full justify-around text-white">
+                            <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="68" height="68"
+                                    viewBox="0 0 32 32">
+                                    <path fill="currentColor"
+                                        d="m16 8l1.43 1.393L11.85 15H24v2H11.85l5.58 5.573L16 24l-8-8z" />
+                                    <path fill="currentColor"
+                                        d="M16 30a14 14 0 1 1 14-14a14.016 14.016 0 0 1-14 14m0-26a12 12 0 1 0 12 12A12.014 12.014 0 0 0 16 4" />
+                                </svg>
+                            </button>
+                            <img class="w-38 h-38 border-8 border-secondary rounded-full"
+                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
+                                alt="profile picture">
+                            <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="68" height="68"
+                                    viewBox="0 0 32 32">
+                                    <path fill="currentColor"
+                                        d="m16 8l-1.43 1.393L20.15 15H8v2h12.15l-5.58 5.573L16 24l8-8z" />
+                                    <path fill="currentColor"
+                                        d="M16 30a14 14 0 1 1 14-14a14.016 14.016 0 0 1-14 14m0-26a12 12 0 1 0 12 12A12.014 12.014 0 0 0 16 4" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="flex flex-col gap-3 items-center">
+                            <cite class="text-xl font-medium text-white">Michael Gough</cite>
+                            <cite class="text-sm text-gray-400">CEO at Google</cite>
+                        </div>
+                    </figcaption>
+                    <svg class="w-10 lg:w-20 h-10 lg:h-20 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                        <path
+                            d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+                    </svg>
+                    <blockquote>
+                        <p class="text-xl lg:text-3xl  italic font-poppins font-medium  text-white">"Flowbite is just
+                            awesome. It
+                            contains tons of predesigned components and pages starting from login screen to complex
+                            dashboard. Perfect choice for your next SaaS application."</p>
+                    </blockquote>
+                </figure>
+
+            </div>
     </section>
 @endsection
