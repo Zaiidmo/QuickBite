4f08d8294b35e82a8e5fdeaf492b15daeedbd12a
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-08 20:49:36 +0000

    BackOffice : Add more links to the dashboard

--- resources/views/layouts/adminComponents.blade.php
+++ resources/views/layouts/adminComponents.blade.php
@@ -177,6 +177,66 @@
             </li>
 
 
+            <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">Restaurants Owners</label>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" viewBox="0 0 24 24">
+                        <path fill="currentColor"
+                            d="m21.96 9.73l-1.43-5a.996.996 0 0 0-.96-.73H4.43c-.45 0-.84.3-.96.73l-1.43 5c-.18.63.3 1.27.96 1.27h2.2L4 20h2l.67-5h10.67l.66 5h2l-1.2-9H21c.66 0 1.14-.64.96-1.27M6.93 13l.27-2h9.6l.27 2zm-2.6-4l.86-3h13.63l.86 3z" />
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">My Restaurants</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" viewBox="0 0 48 48">
+                        <path fill="#f0d5a8"
+                            d="M20.45 13s-.69-2.29 1.09-6.1s5.05-4.68 6.92-3.72a3.17 3.17 0 0 1 1.73 3.2s3.53-.76 4.06 2.08s-1.25 3.89-3.54 4a5.83 5.83 0 0 0-3.55 1.34Z" />
+                        <path fill="#f7e5c6"
+                            d="M30.19 6.42a3.17 3.17 0 0 0-1.73-3.2c-1.83-.94-5.07 0-6.81 3.49c1.22-1.16 2.64-1.91 4-1.52c2.35.69 2.19 2.05 2.19 2.05a5.7 5.7 0 0 1 4.28.69a4.49 4.49 0 0 1 2 2.73a3.69 3.69 0 0 0 .14-2.16c-.54-2.84-4.07-2.08-4.07-2.08" />
+                        <path fill="none" stroke="#45413c" stroke-linecap="round" stroke-linejoin="round"
+                            d="M20.45 13s-.69-2.29 1.09-6.1s5.05-4.68 6.92-3.72a3.17 3.17 0 0 1 1.73 3.2s3.53-.76 4.06 2.08s-1.25 3.89-3.54 4a5.83 5.83 0 0 0-3.55 1.34Z" />
+                        <path fill="#bf8256" stroke="#45413c" stroke-linecap="round" stroke-linejoin="round"
+                            d="M29.34 14.65c0 1.66-2.39 3.8-5.34 3.8s-5.34-2.14-5.34-3.8s2.39-2.24 5.34-2.24s5.34.59 5.34 2.24" />
+                        <path fill="#45413c" d="M7.81 45.06a16.19 1.94 0 1 0 32.38 0a16.19 1.94 0 1 0-32.38 0"
+                            opacity="0.15" />
+                        <path fill="#f0d5a8"
+                            d="M37.22 43a2.08 2.08 0 0 0 1.5-2l-.63-13.18c-.33-6.96-6.54-12.39-14.09-12.39S10.25 20.86 9.92 27.74l-.64 13.18a2.08 2.08 0 0 0 1.51 2a42.78 42.78 0 0 0 26.43.08" />
+                        <path fill="#f7e5c6"
+                            d="m38.24 30.88l-.15-3.14c-.33-6.88-6.54-12.31-14.09-12.31S10.25 20.86 9.92 27.74l-.15 3.14c.9-6.36 7.09-11.22 14.23-11.22s13.34 4.86 14.24 11.22" />
+                        <path fill="none" stroke="#45413c" stroke-linecap="round" stroke-linejoin="round"
+                            d="M37.22 43a2.08 2.08 0 0 0 1.5-2l-.63-13.18c-.33-6.96-6.54-12.39-14.09-12.39S10.25 20.86 9.92 27.74l-.64 13.18a2.08 2.08 0 0 0 1.51 2a42.78 42.78 0 0 0 26.43.08" />
+                        <path fill="none" stroke="#45413c" stroke-linecap="round" stroke-linejoin="round"
+                            d="M20.2 20.61c0-2.86 1.7-5.18 3.8-5.18s3.81 2.32 3.81 5.18" />
+                        <path fill="#ff87af" stroke="#45413c" stroke-linecap="round" stroke-linejoin="round"
+                            d="M30.19 6.42a7.83 7.83 0 0 0-4.37 2.34" />
+                        <path fill="#45413c"
+                            d="M23.35 36.65v-3.43l-1.48-.48a6 6 0 0 1-1.52-.74a3.2 3.2 0 0 1-1-1.13a3.51 3.51 0 0 1-.39-1.73a4.42 4.42 0 0 1 2.19-3.88a5.45 5.45 0 0 1 2.23-.74v-1.17a.47.47 0 0 1 .54-.43c.25 0 .4.08.46.23a2.11 2.11 0 0 1 .09.74v.63a6.6 6.6 0 0 1 2.43.54a1.58 1.58 0 0 1 .78 2.55a1.23 1.23 0 0 1-1 .45a3.2 3.2 0 0 1-1.08-.26a4.84 4.84 0 0 0-1.14-.3v2.74a17.42 17.42 0 0 1 2.36.83a4.17 4.17 0 0 1 1.62 1.33a3.9 3.9 0 0 1 .69 2.43a4.66 4.66 0 0 1-.36 1.85a4.21 4.21 0 0 1-1 1.45a4.9 4.9 0 0 1-1.5 1a5.53 5.53 0 0 1-1.8.44v1.16a.63.63 0 0 1-.15.33a.5.5 0 0 1-.38.13c-.25 0-.4-.08-.47-.24a2.25 2.25 0 0 1-.09-.72v-.64a11.69 11.69 0 0 1-2-.23a4.56 4.56 0 0 1-1.74-.72a1.54 1.54 0 0 1-.73-1.31a1.5 1.5 0 0 1 .38-1a1.27 1.27 0 0 1 1-.42a5.16 5.16 0 0 1 1.49.36a8.17 8.17 0 0 0 1.57.38m0-6.76v-2.35a1.61 1.61 0 0 0-.78.46a1.12 1.12 0 0 0-.31.79a.8.8 0 0 0 .27.63a2.86 2.86 0 0 0 .82.47m1.11 3.67v3a1.72 1.72 0 0 0 1-.6a1.66 1.66 0 0 0 .36-1.06c-.03-.59-.47-1.04-1.36-1.34" />
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">My Earnings</span>
+                </a>
+            </li>
+
+
+            <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">Restaurants Owners</label>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 20V7.1L2.05 2.85L3.85 2L6.2 7.05h11.6L20.15 2l1.8.85L20 7.1V20zm6-7h4q.425 0 .713-.288T15 12t-.288-.712T14 11h-4q-.425 0-.712.288T9 12t.288.713T10 13m-4 5h12V9.05H6zm0 0V9.05z"/></svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Orders</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" viewBox="0 0 26 26"><path fill="currentColor" d="M12.906-.031a1 1 0 0 0-.125.031A1 1 0 0 0 12 1v1H3a3 3 0 0 0-3 3v13c0 1.656 1.344 3 3 3h9v.375l-5.438 2.719a1.006 1.006 0 0 0 .875 1.812L12 23.625V24a1 1 0 1 0 2 0v-.375l4.563 2.281a1.006 1.006 0 0 0 .875-1.812L14 21.375V21h9c1.656 0 3-1.344 3-3V5a3 3 0 0 0-3-3h-9V1a1 1 0 0 0-1.094-1.031M2 5h22v13H2zm18.875 1a1 1 0 0 0-.594.281L17 9.563L14.719 7.28a1 1 0 0 0-1.594.219l-2.969 5.188l-1.219-3.063a1 1 0 0 0-1.656-.344l-3 3a1.016 1.016 0 1 0 1.439 1.44l1.906-1.906l1.438 3.562a1 1 0 0 0 1.812.125l3.344-5.844l2.062 2.063a1 1 0 0 0 1.438 0l4-4A1 1 0 0 0 20.875 6"/></svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">My Statistics</span>
+                </a>
+            </li>
+
+
             <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">User</label>
             <li>
                 <a href="{{ route('profile') }}"
@@ -201,7 +261,7 @@
             <li>
                 <a href="{{ route('profile') }}"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400"  viewBox="0 0 20 20">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" viewBox="0 0 20 20">
                         <path fill="currentColor"
                             d="M9 2a4 4 0 1 0 0 8a4 4 0 0 0 0-8m-4.991 9A2 2 0 0 0 2 13c0 1.691.833 2.966 2.135 3.797C5.417 17.614 7.145 18 9 18q.617 0 1.21-.057A5.48 5.48 0 0 1 9 14.5c0-1.33.472-2.55 1.257-3.5zm6.626 2.92a2 2 0 0 0 1.43-2.478l-.155-.557q.382-.293.821-.497l.338.358a2 2 0 0 0 2.91.001l.324-.344q.448.212.835.518l-.126.423a2 2 0 0 0 1.456 2.519l.349.082a4.7 4.7 0 0 1 .01 1.017l-.46.117a2 2 0 0 0-1.431 2.479l.156.556q-.383.294-.822.498l-.338-.358a2 2 0 0 0-2.909-.002l-.325.344a4.3 4.3 0 0 1-.835-.518l.127-.422a2 2 0 0 0-1.456-2.52l-.35-.082a4.7 4.7 0 0 1-.01-1.016zm4.865.58a1 1 0 1 0-2 0a1 1 0 0 0 2 0" />
                     </svg>
