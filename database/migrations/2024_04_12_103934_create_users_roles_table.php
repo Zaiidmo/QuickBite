be43d1f2ccd1bec1332a3adf59f8898220a7f1db
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-12 10:41:39 +0000

    Database: Create Roles And Permissions Migrations

---
+++ database/migrations/2024_04_12_103934_create_users_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('users_roles', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('role_id');
+    
+            //FOREIGN KEY
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+    
+            //PRIMARY KEYS
+            $table->primary(['user_id','role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_roles');
+    }
+};
