9e7def381660d834eccc2fb7863edf2fa853ae1d
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-13 12:13:36 +0000

    Config :: Binding Restaurant Repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\RestaurantRepository;
+use App\Repositories\RestaurantRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\UserRepositoryInterface;
 use App\Repositories\UserRepository;
@@ -11,5 +13,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(RestaurantRepositoryInterface::class, RestaurantRepository::class);
     }
 }
