479633398b1d37e5be67f3bbe4afa2f68aec5100
Zaiidmo <zaiidmoumnii@gmail.com>
2024-04-16 18:53:06 +0100

    DATABASE: Add Dummy Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,14 +21,45 @@ class DatabaseSeeder extends Seeder
         //Role Seeder
         $this->call(RoleSeeder::class);
 
-        // Super Admin Seeder
+        //Retrieve Roles
         $superAdminRole = Role::where('slug', 'super-admin')->first();
+        $adminRole = Role::where('slug', 'admin')->first();
+        $ownerRole = Role::where('slug', 'restaurant-owner')->first();
+        $deliveryRole = Role::where('slug', 'driver')->first();
+        $customerRole = Role::where('slug', 'customer')->first();
 
+        //Create Users
         $superAdmin = User::create([
             'username' => 'Super Admin',
             'email' => 'super@quickbite.com',
             'password' => bcrypt('password'),
         ]);
+        $admin = User::create([
+            'username' => 'Admin',
+            'email' => 'admin@quickbite.com',
+            'password' => bcrypt('password'),
+        ]);
+        $owner = User::create([
+            'username' => 'Restaurant Owner',
+            'email' => 'owner@quickbite.com',
+            'password' => bcrypt('password'),
+        ]); 
+        $delivery = User::create([
+            'username' => 'Delivery Driver',
+            'email' => 'delivery@quickbite.com',
+            'password' => bcrypt('password'),
+        ]);
+        $customer = User::create([
+            'username' => 'Customer',
+            'email' => 'customer@quickbite.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        //Attach Roles
         $superAdmin->roles()->attach($superAdminRole);
+        $admin->roles()->attach($adminRole);
+        $owner->roles()->attach($ownerRole);
+        $delivery->roles()->attach($deliveryRole);
+        $customer->roles()->attach($customerRole);
     }
 }
